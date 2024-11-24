4f1832c4b45899a66c4836df95e41710c450085b
Alexandra <aleksasha0103@mail.ru>
2024-11-25 02:54:11 +0500

    Internet-shop

---
+++ week11/task2_InternetShop/assets/styles/main.js
@@ -0,0 +1,23 @@
+// const buttonElement = document.querySelector("taskButton");
+// const inputTask = document.getElementById("inputTask");
+// const taskList = document.getElementById("taskList");
+
+// function taskCreation() {
+//   const inputTaskValue = inputTask.value;
+//   const taskListItem = document.createElement("li");
+//   taskListItem.textContent = inputTaskValue;
+//   taskList.append(taskListItem);
+//   inputTask.value = "";
+// }
+
+// taskButton.addEventListener("click", taskCreation);
+
+// function taskChecking() {
+//   taskList.addEventListener("click", function (cross) {
+//     if (cross.target.tagName === "LI") {
+//       cross.target.classList.toggle("crossed");
+//     }
+//   });
+// }
+
+// taskList.addEventListener("click", taskChecking);
