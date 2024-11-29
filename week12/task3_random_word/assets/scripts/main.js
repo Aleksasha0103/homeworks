754df7525e7fd8e51457c00ac349c88476f067b6
Alexandra <aleksasha0103@mail.ru>
2024-11-29 21:45:28 +0500

    Task 3. Week 12

--- week12/task3_random_word/assets/scripts/main.js
+++ week12/task3_random_word/assets/scripts/main.js
@@ -33,5 +33,5 @@ const number3 = Math.floor(Math.random() * alphabet.length);
 const number4 = Math.floor(Math.random() * alphabet.length);
 console.log(`${number1} ${number2} ${number3} ${number4}`);
 
-const randomWord = `${alphabet[number1]} ${alphabet[number2]} ${alphabet[number3]} ${alphabet[number4]}`;
+const randomWord = `${alphabet[number1]}${alphabet[number2]}${alphabet[number3]}${alphabet[number4]}`;
 console.log(randomWord);
