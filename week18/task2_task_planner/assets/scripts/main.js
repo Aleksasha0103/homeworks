3febafda89ec24c045b1fc0fe2d934ed06651341
Alexandra <aleksasha0103@mail.ru>
2025-01-26 23:28:55 +0500

    Homework of the week 18. Task 2. Task planner. Fixing

--- week18/task2_task_planner/assets/scripts/main.js
+++ week18/task2_task_planner/assets/scripts/main.js
@@ -2,41 +2,46 @@ const addingButton = document.getElementById("addingButton");
 const clearingButton = document.getElementById("clearingButton");
 const inputTask = document.getElementById("inputTask");
 const taskList = document.getElementById("taskList");
-const defaultParagraph = document.getElementById("defaultParagraph");
+let defaultParagraph = document.getElementById("defaultParagraph");
 
 function taskCreation() {
-  defaultParagraph.remove();
   const inputTaskValue = inputTask.value;
-  const taskItem = document.createElement("div");
-  taskItem.classList.add("main-part__task-list-field__task-item");
-  const taskItemText = document.createElement("p");
-  taskItemText.classList.add("main-part__task-list-field__task-item-text");
-  taskItemText.textContent = inputTaskValue;
-  const taskItemCheckboxLabel = document.createElement("label");
-  const taskItemCheckbox = document.createElement("input");
-  taskItemCheckbox.classList.add("main-part__task-list-field__task-item-checkbox");
-  taskItemCheckbox.type = "checkbox";
-  const taskItemCustomCheckbox = document.createElement("span");
-  taskItemCustomCheckbox.classList.add("custom-checkbox");
-  taskList.append(taskItem);
-  taskItem.append(taskItemText);
-  taskItem.append(taskItemCheckboxLabel);
-  taskItemCheckboxLabel.append(taskItemCheckbox);
-  taskItemCheckboxLabel.append(taskItemCustomCheckbox);
-  inputTask.value = "";
-  clearingButton.className = "main-part__button__clearing-button-active";
+  if (inputTaskValue === "") {
+    alert("Введите текст задачи!");
+    return;
+  } else {
+    defaultParagraph.remove();
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("main-part__task-list-field__task-item");
+    const taskItemText = document.createElement("p");
+    taskItemText.classList.add("main-part__task-list-field__task-item-text");
+    taskItemText.textContent = inputTaskValue;
+    const taskItemCheckboxLabel = document.createElement("label");
+    const taskItemCheckbox = document.createElement("input");
+    taskItemCheckbox.classList.add("main-part__task-list-field__task-item-checkbox");
+    taskItemCheckbox.type = "checkbox";
+    const taskItemCustomCheckbox = document.createElement("span");
+    taskItemCustomCheckbox.classList.add("custom-checkbox");
+    taskList.append(taskItem);
+    taskItem.append(taskItemText);
+    taskItem.append(taskItemCheckboxLabel);
+    taskItemCheckboxLabel.append(taskItemCheckbox);
+    taskItemCheckboxLabel.append(taskItemCustomCheckbox);
+    inputTask.value = "";
+    clearingButton.className = "main-part__button__clearing-button-active";
+  }
 }
 
 addingButton.addEventListener("click", taskCreation);
 
 function listCleareance() {
   taskList.innerHTML = "";
+  clearingButton.className = "main-part__button__clearing-button";
   defaultParagraph = document.createElement("p");
   defaultParagraph.id = "defaultParagraph";
   defaultParagraph.classList.add("main-part__task-list-field__default-text");
   defaultParagraph.textContent = "Нет задач";
   taskList.append(defaultParagraph);
-  clearingButton.className = "main-part__button__clearing-button";
 }
 
 clearingButton.addEventListener("click", listCleareance);
