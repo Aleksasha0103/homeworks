7d55e97f79577b0344c9cd1af059d4f7ab7bf82e
Alexandra <aleksasha0103@mail.ru>
2025-02-09 01:57:15 +0500

    Homework of the week 20. Star Wars

---
+++ week20/task2_StarWars/assets/scripts/main.js
@@ -0,0 +1,43 @@
+const searchButton = document.getElementById("searchButton");
+const objectSelect = document.getElementById("objectSelect");
+const numberSelect = document.getElementById("numberSelect");
+const resultField = document.getElementById("resultField");
+const errorField = document.getElementById("errorField");
+const finallyField = document.getElementById("finallyField");
+
+searchButton.addEventListener("click", () => {
+  resultField.textContent = "Идёт загрузка данных. Пожалуйста, подождите.";
+  errorField.textContent = "";
+  finallyField.textContent = "";
+
+  let objectChosen = objectSelect.value;
+  let numberChosen = numberSelect.value;
+
+  fetch(`https://swapi.py4e.com/api/${objectChosen}/${numberChosen}`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Статус запроса: произошла ошибка (${response.status}).`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.name) {
+        resultField.textContent = JSON.stringify(data.name);
+      } else {
+        resultField.textContent = JSON.stringify(data.title);
+      }
+      errorField.textContent = "";
+    })
+    .catch((error) => {
+      errorField.innerHTML = `Запрос отклонён. Вероятно, запрашиваемые данные отсутствуют на сервере. <br>${error.message}`;
+      resultField.textContent = "";
+    })
+    .finally(function () {
+      finallyField.innerHTML = `Поиск завершён. <br>При необходимости вы можете попробовать обратиться к серверу ещё раз.`;
+    });
+});
