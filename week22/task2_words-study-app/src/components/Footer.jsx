9539dd32fdde16e8a46ba2a565571d751a76cd41
Aleksasha0103 <aleksasha0103@mail.ru>
2025-03-31 02:36:34 +0500

    Homework of the week 22. Task 2

---
+++ week22/task2_words-study-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <section className="footerBlock">
+      <p>Copyright © 2025. Все права защищены</p>
+    </section>
+  );
+}
+
+export default Footer;
---
+++ week22/task2_words-study-app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <section className="headerBlock">
+      <img src="#" alt="logo"></img>
+      <div className="headerButtonsContainer">
+        <button className="headerButton">Вход</button>
+        <button className="headerButton">Регистрация</button>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
