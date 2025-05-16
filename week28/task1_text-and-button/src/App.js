e2b173e06b1eab01b05ffb3f978411803cab53f0
Aleksasha0103 <aleksasha0103@mail.ru>
2025-05-16 19:59:43 +0500

    Homework of the week 28. Task1, Text and button

---
+++ week28/task1_text-and-button/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import TextAndButton from "./components/TextAndButton";
+
+function App() {
+  return <TextAndButton />;
+}
+
+export default App;
---
+++ week28/task1_text-and-button/src/components/TextAndButton.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "../styles/styles.css";
+
+function TextAndButton() {
+  const [text, setText] = useState("");
+  const [finalText, setfinalText] = useState("");
+
+  const putText = (e) => {
+    setText(e.target.value);
+  };
+
+  const putfinalText = (e) => {
+    e.preventDefault();
+    setfinalText(text);
+    setText("");
+  };
+
+  return (
+    <>
+      <form className="main__wrapper">
+        <textarea onChange={putText} value={text} placeholder="Введите текст"></textarea>
+        <button type="submit" onClick={putfinalText}>
+          Записать
+        </button>
+        <div className="text-paragraph">{finalText}</div>
+      </form>
+    </>
+  );
+}
+
+export default TextAndButton;
