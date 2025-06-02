810e27c1db3a30a29599e28c5dd5c130dd99bb5a
Aleksasha0103 <aleksasha0103@mail.ru>
2025-06-03 01:49:42 +0500

    Homework of the week 30. Task1, Words-study-app

---
+++ week30/task1_words-study-app/src/components/common/LanguageChoice.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import languagesList from "../../data/languages";
+import { fetchVocabularyEnglish } from "../../data/vocabularyEnglish";
+import "../../styles/styles.scss";
+
+function LanguageChoice({ setVocabulary, setChooseLanguage }) {
+  const chosenLanguageWord = async (language) => {
+    setChooseLanguage(language);
+
+    if (language === "Английский") {
+      try {
+        const englishVocabulary = await fetchVocabularyEnglish();
+        setVocabulary(englishVocabulary);
+      } catch (error) {
+        console.error("Failed to load vocabulary: ", error);
+        setVocabulary([]);
+      }
+    } else {
+      setVocabulary([]);
+    }
+  };
+
+  return (
+    <section className="LanguageChoiceContainer">
+      {languagesList.map(({ id, name }) => (
+        <button key={id} className="languageChoiceButton" onClick={() => chosenLanguageWord(name)}>
+          {name}
+        </button>
+      ))}
+    </section>
+  );
+}
+
+export default LanguageChoice;
